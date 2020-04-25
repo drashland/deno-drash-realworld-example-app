3d2c69a252dd85e4e078818ef20689d791d31894
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-25 05:27:47 +0100

    Added basic middlewares

---
+++ src/middlewares/auth_middleware.ts
@@ -0,0 +1,7 @@
+import { Drash } from "../deps.ts";
+
+export default class AuthMiddleware extends Drash.Http.Middleware {
+    public run() {
+        // TODO :: Check if authed user is set
+    }
+}
