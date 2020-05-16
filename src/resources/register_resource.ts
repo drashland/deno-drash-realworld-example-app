73a909d7e96ebbda0d8f860eedb71d5f16c33c20
Edward Bebbington <edward.bebbington@intercity.technology>
2020-05-16 04:29:05 +0100

    Fixed register resource getting body data

--- src/resources/register_resource.ts
+++ src/resources/register_resource.ts
@@ -28,10 +28,9 @@ class RegisterResource extends Drash.Http.Resource {
      */
     public async POST() {
         // Gather data
-        const username = this.request.getBodyParam('username')
-        const email = this.request.getBodyParam('email')
-        const password = this.request.getBodyParam('password')
-
+        const username = decodeURIComponent(this.request.getBodyParam('username'))
+        const email = decodeURIComponent(this.request.getBodyParam('email'))
+        const password = decodeURIComponent(this.request.getBodyParam('password'))
         // Validate
         const userModel = new UserModel();
         const validation = await userModel.validate({username, email, password})
@@ -40,7 +39,6 @@ class RegisterResource extends Drash.Http.Resource {
             this.response.body = JSON.stringify(validation)
             return this.response
         }
-
         // Hash password
         // TODO :: When and if it's available within Deno, or an external module (must include hash matching too)
 
