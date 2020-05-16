00b21b689df5e750e155e8fbad9d724ff3531d5f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-05-16 04:28:31 +0100

    Finished js client file

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -5,11 +5,12 @@ $(document).ready(function () {
     const $password = $('#password')
     // Clean up the form
     $username.css('border', '')
-    $username.css('border', '')
+    $email.css('border', '')
     $password.css('border', '')
     $('#username + p').text('')
     $('#email + p').text('')
     $('#password + p').text('')
+    $('form p.success-message').text('')
     // Gather data
     const [username, email, password] = [$username.val(), $email.val(), $password.val()]
     // Send request
@@ -31,14 +32,19 @@ $(document).ready(function () {
     } catch (err) {
       alert('Error with this request. See console')
       console.error(res)
+      return false
     }
     // If failed validation
     if (res.success === false) {
       // Highlight the errored field
       $('#' + res.data).css('border', '1px solid red')
       $('#' + res.data + ' + p').text(res.message)
+      return false
     }
     // When passed validation
-    window.location.href = '/login'
+    $('form p.success-message').text(res.message)
+    $username.val('')
+    $email.val('')
+    $password.val('')
   })
 })
