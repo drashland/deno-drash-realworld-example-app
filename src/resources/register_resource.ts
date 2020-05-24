289f60746151537414962635dfaeaf6c099fc596
Edward Bebbington <edward.bebbington@intercity.technology>
2020-05-24 20:48:33 +0100

    [hash-passwords] now hashing passwords on register

--- src/resources/register_resource.ts
+++ src/resources/register_resource.ts
@@ -1,4 +1,4 @@
-import { Drash } from "../deps.ts"
+import { Drash, bcrypt } from "../deps.ts"
 import UserModel from "../models/user_model.ts";
 
 class RegisterResource extends Drash.Http.Resource {
@@ -30,18 +30,17 @@ class RegisterResource extends Drash.Http.Resource {
         // Gather data
         const username = decodeURIComponent(this.request.getBodyParam('username'))
         const email = decodeURIComponent(this.request.getBodyParam('email'))
-        const password = decodeURIComponent(this.request.getBodyParam('password'))
+        const rawPassword = decodeURIComponent(this.request.getBodyParam('password'))
         // Validate
         const userModel = new UserModel();
-        const validation = await userModel.validate({username, email, password})
+        const validation = await userModel.validate({username, email, password: rawPassword})
         if (validation.success === false) {
             // TODO :: Is an error code useful here?
             this.response.body = JSON.stringify(validation)
             return this.response
         }
         // Hash password
-        // TODO :: When and if it's available within Deno, or an external module (must include hash matching too)
-
+        const password = await bcrypt.hash(rawPassword)
         // Create user
         await userModel.CREATE(UserModel.CREATE_ONE, [username, email, password])
         this.response.body = JSON.stringify({success: true, message: 'Successfully created.'})
