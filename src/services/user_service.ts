694f5e02b4503fc6d06fe4b79f94be1b1639a89d
Eric Crooks <eric.crooks@gmail.com>
2020-05-27 20:29:54 -0400

    [issue-#31-login-register-resources] implement getUserByUsername

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,9 +1,9 @@
 import UserModel from "../models/user_model.ts";
+const model = new UserModel();
 
 class UserService {
   static async getUserByEmail(email: string) {
-    console.log(`Getting user from database with "${email}" as the email address.`);
-    const model = new UserModel();
+    console.log(`Getting user from database by email "${email}".`);
     const user = await model.SELECT(UserModel.SELECT_ALL_BY_EMAIL, [email]);
     console.log(user[0]);
     if (user.length) {
@@ -13,15 +13,13 @@ class UserService {
   }
 
   static getUserByUsername(username: string) {
-    return {
-      "user": {
-        "email": "user1",
-        "token": "jwt.token.here",
-        "username": "user1",
-        "bio": "I am a developer.",
-        "image": null
-      }
+    console.log(`Getting user from database by username "${username}".`);
+    const user = await model.SELECT(UserModel.SELECT_ALL_BY_USERNAME, [username]);
+    console.log(user[0]);
+    if (user.length) {
+      return user[0];
     }
+    return null;
   }
 
   static getUserProfileByUsername(username: string) {
