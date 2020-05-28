546364d75578466fa9ede2bae3040565335dfb59
Eric Crooks <eric.crooks@gmail.com>
2020-05-28 17:32:09 -0400

    [issue-#27-settings-page] add keep-alive tag

--- src/vue/components/App.vue
+++ src/vue/components/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
     <Footer />
   </div>
 </template>
