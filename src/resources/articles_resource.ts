f42d2981e1879cd672755fd13cdc6070698bc453
Eric Crooks <eric.crooks@gmail.com>
2020-05-29 23:23:03 -0400

    [issue-#42-user-favorited-articles] mock data for now since db doesn't exist

--- src/resources/articles_resource.ts
+++ src/resources/articles_resource.ts
@@ -1,15 +1,56 @@
 import { Drash } from "../deps.ts"
-import ArticleService from "../services/article_service.ts";
+// import ArticleService from "../services/article_service.ts";
 
 class ArticlesResource extends Drash.Http.Resource {
 
   static paths = [
     "/articles",
-    "/articles/:id",
+    "/articles/:slug",
   ];
 
   public GET() {
-    this.response.body = [];
+    this.response.body = {
+      "article": {
+        "slug": "how-to-train-your-dragon",
+        "title": "How to train your dragon",
+        "description": "Ever wonder how?",
+        "body": "It takes a Jacobian",
+        "tagList": ["dragons", "training"],
+        "createdAt": "2016-02-18T03:22:56.637Z",
+        "updatedAt": "2016-02-18T03:48:35.824Z",
+        "favorited": false,
+        "favoritesCount": 0,
+        "author": {
+          "username": "jake",
+          "bio": "I work at statefarm",
+          "image": "https://i.stack.imgur.com/xHWG8.jpg",
+          "following": false
+        }
+      }
+    };
+    return this.response;
+  }
+
+  public POST() {
+    this.response.body = {
+      "article": {
+        "slug": "how-to-train-your-dragon",
+        "title": "How to train your dragon",
+        "description": "Ever wonder how?",
+        "body": "It takes a Jacobian",
+        "tagList": ["dragons", "training"],
+        "createdAt": "2016-02-18T03:22:56.637Z",
+        "updatedAt": "2016-02-18T03:48:35.824Z",
+        "favorited": false,
+        "favoritesCount": 0,
+        "author": {
+          "username": "jake",
+          "bio": "I work at statefarm",
+          "image": "https://i.stack.imgur.com/xHWG8.jpg",
+          "following": false
+        }
+      }
+    };
     return this.response;
   }
 }
