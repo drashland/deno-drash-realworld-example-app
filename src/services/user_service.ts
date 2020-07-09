03728c4397e17cddac6d7994024101a6663ffd6a
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-09 20:38:33 +0100

    [add-comment-functionality] Added user service to aid in getting logged in user

---
+++ src/services/user_service.ts
@@ -0,0 +1,33 @@
+import UserModel from "../models/user_model.ts";
+import SessionModel from "../models/session_model.ts";
+
+export default class UserService {
+  /**
+   * Get user model for logged in user
+   *
+   * @return UserModel|boolean
+   *     - Returns the user model.
+   *     - Returns false if:
+   *       - No "drash_sess" cookie
+   *       - No session exists with the "drash_sess" cookie
+   *       - No user was found with that session
+   */
+  static async getLoggedInUser(): Promise<UserModel|boolean> {
+    const sessionCookie = this.request.getCookie("drash_sess");
+    if (!sessionCookie) {
+      return false
+    }
+    const sessionOne = sessionCookie.split("|::|")[0];
+    const sessionTwo = sessionCookie.split("|::|")[1];
+    const session = await SessionModel.getUserSession(sessionOne, sessionTwo)
+    if (!session) {
+      return false
+    }
+    const userId = session.id
+    const user = await UserModel.where({ id: userId })
+    if (!user.length) {
+      return false
+    }
+    return user
+  }
+}
