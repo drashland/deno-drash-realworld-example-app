440257dac28ef0adf14f8ecd56856a541bec9c6e
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-09 21:11:37 +0100

    [add-comment-functionality] Completed article comments resource

--- src/resources/article_comments_resource.ts
+++ src/resources/article_comments_resource.ts
@@ -1,30 +1,95 @@
 import { Drash } from "../deps.ts";
+import {ArticleModel} from "../models/article_model.ts";
+import {ArticleCommentsModel} from "../models/article_comments_model.ts";
+import UserService from "../services/user_service.ts";
 // import ArticleService from "../services/article_service.ts";
 
-class ArticleCommentsResource extends Drash.Http.Resource {
+export default class ArticleCommentsResource extends Drash.Http.Resource {
   static paths = [
     "/articles/:slug/comments",
   ];
 
-  public GET() {
-    this.response.body = [
-      {
-        "comment": {
-          "id": 1,
-          "createdAt": "2016-02-18T03:22:56.637Z",
-          "updatedAt": "2016-02-18T03:22:56.637Z",
-          "body": "It takes a Jacobian",
-          "author": {
-            "username": "jake",
-            "bio": "I work at statefarm",
-            "image": "https://i.stack.imgur.com/xHWG8.jpg",
-            "following": false,
-          },
-        },
-      },
-    ];
+  public async GET() {
+    const slug = this.request.getQueryParam("slug");
+    const article = await ArticleModel.where({ slug })
+    if (!article.length) {
+      this.response.status_code = 404
+      this.response.body = {
+        errors: {
+          comment: "No article was found for the given article"
+        }
+      }
+      return this.response
+    }
+    const comments = await ArticleCommentsModel.whereIn("article_id", [article.id]);
+    if (!comments.length) {
+      this.response.status_code = 404
+      this.response.body = {
+        errors: {
+          comment: "No comments were found for the given article"
+        }
+      }
+      return this.response
+    }
+    this.response.body = comments;
     return this.response;
   }
-}
 
-export default ArticleCommentsResource;
+  public async POST() {
+    console.log('Handling ArticleCommentsResource POST.')
+    const comment = this.request.getBodyParam("comment")
+    const slug = this.request.getUrlQueryParam("slug")
+    // First find an article by that slug. The article should exist.
+    const articles = await ArticleModel.where({ slug })
+    if (!articles.length) {
+      console.error("Article does not exist for the posted comment.");
+      this.response.status_code = 422;
+      this.response.body = {
+        errors: {
+          comment: "No article was found"
+        }
+      }
+      return this.response
+    }
+    const article = articles[0]
+    // Get user and validation check
+    if (!comment) {
+      this.response.status_code = 422
+      this.response.body = {
+        errors: {
+          comment: "A comment is required to post"
+        }
+      }
+      return this.response
+    }
+    const user = await UserService.getLoggedInUser();
+    if (!user) {
+      this.response.status_code = 403
+      this.response.body  =  {
+        errors: {
+          comment: "You are unauthorised to do this action."
+        }
+      }
+      return this.response
+    }
+    // save the comment
+    const articleComment = new ArticleCommentsModel(article.id, comment, user.image, user.id, user.username)
+    const savedArticleComment = await articleComment.save()
+    if (!savedArticleComment) {
+      this.response.status_code = 500
+      this.response.body =  {
+        errors: {
+          comment: "Failed to save the comment"
+        }
+      }
+      return this.response
+    }
+    this.response.status_code = 200
+    this.response.body = {
+      data: {
+        success: true
+      }
+    }
+    return this.response
+  }
+}
