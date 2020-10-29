8c84384d3100114ed745ab4721eb2aeaed593a47
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-29 13:05:28 +0000

    [cleanup-for-v1] Use bumper service to update drash dockerfile

--- .docker/update_deno_version_in_dockerfile.ts
+++ .docker/update_deno_version_in_dockerfile.ts
@@ -1,18 +1,11 @@
-const fetchRes = await fetch("https://cdn.deno.land/deno/meta/versions.json");
-const versions: {
-  latest: string;
-  versions: string[];
-} = await fetchRes.json(); // eg { latest: "v1.3.3", versions: ["v1.3.2", ...] }
-const latestDenoVersion = versions.latest.replace("v", "");
+import { BumperService } from "https://raw.githubusercontent.com/drashland/services/master/ci/bumper_service.ts";
 
-let dockerfileContent = new TextDecoder().decode(
-  await Deno.readFile("./.docker/drash.dockerfile"),
-);
-dockerfileContent = dockerfileContent.replace(
-  /-s v[0-9.]+[0-9.]+[0-9]/g,
-  `-s v${latestDenoVersion}`,
-);
-await Deno.writeFile(
-  "./.docker/drash.dockerfile",
-  new TextEncoder().encode(dockerfileContent),
-);
+const b = new BumperService("realworld")
+
+b.bump([
+  {
+    filename: "./.docker/drash.dockerfile",
+    replaceTheRegex: /-s v[0-9.]+[0-9.]+[0-9]/g,
+    replaceWith: "-s v{{ latestDenoVersion }}",
+  }
+]);
