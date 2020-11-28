a9597ae0fa0a8b99442304e1680efe86ecc09ded
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-28 19:32:49 +0000

    deno fmt D:

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,9 @@
 export { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts";
 
 export { connectPg } from "https://deno.land/x/pgc4d@v1.3.6/src/mod.ts";
-export type { KeyedRow, PgConn } from "https://deno.land/x/pgc4d@v1.3.6/src/mod.ts";
+export type {
+  KeyedRow,
+  PgConn,
+} from "https://deno.land/x/pgc4d@v1.3.6/src/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export { bcrypt };
