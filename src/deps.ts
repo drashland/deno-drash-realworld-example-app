d7fefa88cb56a549da0756dc8f6ca8f6f36c4c12
crookse <crookse@users.noreply.github.com>
2021-01-22 01:13:18 +0000

    Update dependencies and bump version numbers across repo

--- src/deps.ts
+++ src/deps.ts
@@ -1,11 +1,11 @@
-export { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.0/mod.ts";
 
 export {
   Client as PostgresClient,
   Pool,
-} from "https://deno.land/x/postgres@v0.4.5/mod.ts";
-export { Column } from "https://deno.land/x/postgres@v0.4.5/connection.ts";
-export type { QueryResult } from "https://deno.land/x/postgres@v0.4.5/query.ts";
-export { PoolClient } from "https://deno.land/x/postgres@v0.4.5/client.ts";
+} from "https://deno.land/x/postgres@v0.4.6/mod.ts";
+export { Column } from "https://deno.land/x/postgres@v0.4.6/connection.ts";
+export type { QueryResult } from "https://deno.land/x/postgres@v0.4.6/query.ts";
+export { PoolClient } from "https://deno.land/x/postgres@v0.4.6/client.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export { bcrypt };
