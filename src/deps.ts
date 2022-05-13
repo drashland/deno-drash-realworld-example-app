280deaba9698fbfda926533761bbb7426754cf72
Edward Bebbington <EdwardSBebbington@hotmail.com>
2022-05-13 21:40:57 +0100

    upgrade vital

--- src/deps.ts
+++ src/deps.ts
@@ -7,4 +7,4 @@ export {
   ClientPostgreSQL,
 } from "https://deno.land/x/nessie@2.0.4/mod.ts";
 export type { Info } from "https://deno.land/x/nessie@2.0.4/mod.ts";
-export { Model, QueryBuilder } from "https://deno.land/x/vital@v1.1.0/mod.ts";
+export { Model, QueryBuilder } from "https://deno.land/x/vital@v1.1.1/mod.ts";
