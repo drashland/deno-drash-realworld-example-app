dfcea635ade2dedb4a88ddad3c71f266ad259595
drashbot <59404681+drashbot@users.noreply.github.com>
2022-05-28 00:39:43 +0000

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,10 @@
-export * as Drash from "https://deno.land/x/drash@v2.2.0/mod.ts";
-export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
-export { TengineService } from "https://deno.land/x/drash@v2.2.0/src/services/tengine/tengine.ts";
+export * as Drash from "https://deno.land/x/drash@v2.6.0/mod.ts";
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.3.0/mod.ts";
+export { TengineService } from "https://deno.land/x/drash@v2.6.0/src/services/tengine/tengine.ts";
 export {
   AbstractMigration,
   AbstractSeed,
   ClientPostgreSQL,
-} from "https://deno.land/x/nessie@2.0.4/mod.ts";
-export type { Info } from "https://deno.land/x/nessie@2.0.4/mod.ts";
+} from "https://deno.land/x/nessie@2.0.6/mod.ts";
+export type { Info } from "https://deno.land/x/nessie@2.0.6/mod.ts";
 export { Model, QueryBuilder } from "https://deno.land/x/vital@v1.1.1/mod.ts";
