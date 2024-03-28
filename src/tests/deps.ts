c3d0970ee1c4e0ce5dbca2e70439397e4708058c
drashbot <59404681+drashbot@users.noreply.github.com>
2024-03-28 00:32:24 +0000

    chore: update deps

--- src/tests/deps.ts
+++ src/tests/deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@v0.141.0/testing/asserts.ts";
-export { State } from "https://deno.land/x/nessie@2.0.6/cli/state.ts";
+export { assertEquals } from "https://deno.land/std@v0.221.0/testing/asserts.ts";
+export { State } from "https://deno.land/x/nessie@2.0.11/cli/state.ts";
